75e3c6cdf3b51621e8ccc4603ab178e7d65d3e21
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-05-12 11:53:56 +0100

    api-client init

--- server/src/main/java/com/task/server/config/ApplicationConfig.java
+++ server/src/main/java/com/task/server/config/ApplicationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.task.server.interceptors.JwtInterceptor;
 
