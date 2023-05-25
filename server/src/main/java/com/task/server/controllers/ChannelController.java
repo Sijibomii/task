9e84ef84e8bad275f17e493a01e6aca5b15ace68
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-05-25 04:33:07 +0100

    channel controller routes

--- server/src/main/java/com/task/server/controllers/ChannelController.java
+++ server/src/main/java/com/task/server/controllers/ChannelController.java
@@ -18,21 +18,25 @@ import jakarta.servlet.http.HttpServletResponse;
 @RestController
 @Controller
 public class ChannelController extends BaseController{
+
     
-    // get all channels assigned to this org. Note: return only channels user is a memeber of except user has see all channels perm
-    @RequiresPermissions("channel:create:organization:{orgId}")
+    // this is for getting a particular channel and its messages. should not return messages if sending user is not a memeber
+
+
+    // get all channels mapped to the org
     @RequestMapping(value = "/channels/organization/{id}", method = RequestMethod.GET)
     public MessageResult getChannelsOrg(HttpServletRequest request, HttpServletResponse response) throws Exception{
         String userId = (String) request.getAttribute("userId");
         if (userId.isEmpty()){
             throw new Exception("Auth error");
         }
-        
+        // get all channels assigned to this org. Note: return only channels user is a memeber of except user has see all channels perm
         return success();
     }
 
-    // check if user has channel create permission
-    @RequestMapping(value = "/channels/organization/{id}", method = RequestMethod.POST)
+    // check if user has channel create permission mapped to this org
+    @RequiresPermissions("channel:create:organization:{orgId}")
+    @RequestMapping(value = "/channels/organization/{orgId}", method = RequestMethod.POST)
     public MessageResult createChannelsOrg(HttpServletRequest request, HttpServletResponse response) throws Exception{
         String userId = (String) request.getAttribute("userId");
         if (userId.isEmpty()){
@@ -42,6 +46,7 @@ public class ChannelController extends BaseController{
         return success();
     }
 
+    // get all channels mapped to the team of course only ones user is a member of except you are the creator
     @RequestMapping(value = "/channels/team/{id}", method = RequestMethod.GET)
     public MessageResult getChannelsTeam(HttpServletRequest request, HttpServletResponse response) throws Exception{
         String userId = (String) request.getAttribute("userId");
@@ -52,7 +57,8 @@ public class ChannelController extends BaseController{
         return success();
     }
 
-    @RequestMapping(value = "/channels/team/{id}", method = RequestMethod.POST)
+    @RequiresPermissions("channel:create:team:{teamId}")
+    @RequestMapping(value = "/channels/team/{teamId}", method = RequestMethod.POST)
     public MessageResult createChannelsTeam(HttpServletRequest request, HttpServletResponse response) throws Exception{
         String userId = (String) request.getAttribute("userId");
         if (userId.isEmpty()){
@@ -62,7 +68,8 @@ public class ChannelController extends BaseController{
         return success();
     }
 
-    @RequestMapping(value = "/channels/project/{id}", method = RequestMethod.POST)
+    @RequiresPermissions("channel:create:project:{projectId}")
+    @RequestMapping(value = "/channels/project/{projectId}", method = RequestMethod.POST)
     public MessageResult createChannelsProject(HttpServletRequest request, HttpServletResponse response) throws Exception{
         String userId = (String) request.getAttribute("userId");
         if (userId.isEmpty()){
