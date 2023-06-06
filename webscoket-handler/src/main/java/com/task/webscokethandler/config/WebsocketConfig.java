ee8377e67f47a56e3feb61e6ae291c799b93092d
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-06-06 04:37:38 +0100

    initial config

--- webscoket-handler/src/main/java/com/task/webscokethandler/config/WebsocketConfig.java
+++ webscoket-handler/src/main/java/com/task/webscokethandler/config/WebsocketConfig.java
@@ -2,28 +2,25 @@ package com.task.webscokethandler.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Configure message broker options
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic","/user");
+        config.setUserDestinationPrefix("/user/");
+        config.setApplicationDestinationPrefixes("/app");
+    } 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Register the endpoint for clients to connect to
-        registry.addEndpoint("/websocket")
-                .setAllowedOrigins("*")
-                .setHandshakeHandler(new CustomHandshakeHandler())
-                .withSockJS();
+        registry.addEndpoint("/chat-webSocket").setAllowedOrigins("*").withSockJS();
     }
-}
 
+
+}
