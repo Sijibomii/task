8dc6a52ff76cea782b1985fae07485f54fd68b14
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-06-08 08:43:51 +0100

    webhandler controller init

--- webscoket-handler/src/main/java/com/task/webscokethandler/config/StartUpConfig.java
+++ webscoket-handler/src/main/java/com/task/webscokethandler/config/StartUpConfig.java
@@ -1,16 +1,21 @@
 package com.task.webscokethandler.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.kafka.core.KafkaTemplate;
 
 // on start up web handler should announce to the server that it just came online through kafka. 
 // users request a web handler ip and server returns one from it
 @Component
 public class StartUpConfig implements CommandLineRunner{
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
     
     @Override
     public void run(String... args) throws Exception {
-        // Code to be executed when the application starts up
+        // broadcast to kafka that a webhandler is online
         System.out.println("Application started!#############################################");
     }
 
