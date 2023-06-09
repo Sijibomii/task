7b43d83d012c53745ac7d9819dad1f6960854ea1
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-06-09 03:07:16 +0100

    entities

--- api-client/src/entities.ts
+++ api-client/src/entities.ts
@@ -1,7 +1,28 @@
 
 export type UUID = string;
 
+// organization
+export type Orgaanization ={
+    id: string;
+    members_count: number;
+};
+
+// projects
+export type Project ={
+    id: string;
+    members_count: number;
+};
+
+// teams
+export type Team ={
+    id: string;
+    members_count: number;
+};
+
 export type User = {
     username: string;
     online: boolean;
+    orgs: Orgaanization[];
+    teams: Team[];
+    projects: Project[];
   };
