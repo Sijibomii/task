33f1dd3a8f7af6c38526aaa4dd6b244e1feaabc0
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-20 22:21:06 +0100

    tablet sidebar init

--- web-client/src/modules/layouts/TabletSidebar.tsx
+++ web-client/src/modules/layouts/TabletSidebar.tsx
@@ -1,6 +1,9 @@
+import Bell from "@/icons/Bell";
+import Chat from "@/icons/Chat";
+import CheckSquare from "@/icons/CheckSquare";
+import People from "@/icons/People";
+import Settings from "@/icons/Settings";
 import React, { useState } from "react";
-
-
  
 export const TabletSidebar: React.FC<any> = ({}) => {
   
@@ -8,7 +11,13 @@ export const TabletSidebar: React.FC<any> = ({}) => {
   return (
     <>
       <div className="">
-        
+        <div className="flex flex-col py-4">
+          <CheckSquare className="text-white my-4" width={32} height={32} />
+          <Chat className="text-white my-4" width={32} height={32} />
+          <Bell className="text-white my-4" width={32} height={32} />
+          <People className="text-white my-4" width={32} height={32} />
+          <Settings className="text-white my-4" width={32} height={32} />
+        </div>
       </div>
     </>
   );
