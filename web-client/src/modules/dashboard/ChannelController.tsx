35ac1a7809aeb31aff41e2c9cb21a2cb79c1111a
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-23 03:12:09 +0100

    channel controller..

--- web-client/src/modules/dashboard/ChannelController.tsx
+++ web-client/src/modules/dashboard/ChannelController.tsx
@@ -1,3 +1,10 @@
+import People from "@/icons/People";
+import PersonPlus from "@/icons/PersonPlus";
+import SvgSolidSearch from "@/icons/SolidSearch";
+import Telephone from "@/icons/Telephone";
+import ThreeHorizontalDots from "@/icons/ThreeDotsHor";
+import Video from "@/icons/Video";
+import { Tag } from "@/ui/Tag";
 import React from "react";
 
 
@@ -11,6 +18,31 @@ interface ChannelProps {
 export const ChannelController: React.FC<ChannelProps> = ({ children }) => {
 
   return (
-    <></>
+    <div className="">
+        <div className="">
+            {/* header */}
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <h3>Desginer</h3>
+                        <Tag />
+                    </div>
+
+                    <div className="">
+                        <SvgSolidSearch />
+                        <PersonPlus />
+                        <Telephone />
+                        <Video />
+                        <ThreeHorizontalDots />
+                    </div>
+                </div>
+
+                <div className="">
+                    <People />
+                    <h6>10 Members</h6>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 };
