6b43a4a370262f30d1b3a55159e6b39aa667e5cf
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-28 05:27:27 +0100

    chat init

---
+++ server/src/main/java/com/task/server/entity/Chat.java
@@ -0,0 +1,22 @@
+package com.task.server.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Data
+@Table
+public class Chat {
+    
+}
