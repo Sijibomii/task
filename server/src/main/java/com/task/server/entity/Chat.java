8682742886f2f83d7d37f01a68dd6adf3deba21f
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-07-29 03:20:09 +0100

    chat seed db

--- server/src/main/java/com/task/server/entity/Chat.java
+++ server/src/main/java/com/task/server/entity/Chat.java
@@ -1,22 +1,35 @@
 package com.task.server.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import java.util.UUID;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 @Entity
 @Data
 @Table
 public class Chat {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    @ManyToOne
+    private Users creator;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @CreationTimestamp
+    private Date createdOn;
+
+    private Channel channel;
 }
---
+++ server/src/main/java/com/task/server/seed/chat.db.sql
@@ -0,0 +1,9 @@
+-- ----------------------------
+-- Records of channels
+-- ----------------------------
+
+INSERT INTO chat (id, creator_id, createdOn, channel_id)
+VALUES
+  ('uuid_chat1', 'uuid_user1', '2023-07-18 12:34:56', 'uuid_channel1'),
+  ('uuid_chat2', 'uuid_user3', '2023-07-18 12:34:56', 'uuid_channel2'),
+;
